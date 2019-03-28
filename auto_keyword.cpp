e6cc371e50bdc0dcd63e1998048bc910266d1c01
davylin <davylin@163.com>
2019-03-29 02:47:41 +0800

    modified

--- auto_keyword.cpp
+++ auto_keyword.cpp
@@ -1,27 +1,54 @@
-nclude <iostream>
+//auto_keyword.cpp
+//For C++14
+// g++ -std=c++14 -o a auto_keyword.cpp
+
+#include <iostream>
 #include <vector>
 using namespace std;
 
+//C++ 11
 template<class T, class U>
 void add(T t, U u)
 {
     auto s = t + u;
-    cout << "type of t + u is " << typeid(s).name() << endl;
+    cout << "type of t + u is " << typeid(s).name() << endl;  //ÔÚc++ÖĞ£¬typeidÓÃÓÚ·µ»ØÖ¸Õë»òÒıÓÃËùÖ¸¶ÔÏóµÄÊµ¼ÊÀàĞÍ
+}
+
+//C++ 11
+template<class T, class U>
+auto add2(T t, U u) -> decltype(t+u)
+{
+    auto s = t + u;
+    return s;
+}
+
+//C++ 14
+template<class T, class U>
+decltype(auto) add3(T t, U u)
+{
+    auto s = t + u;
+    return s;
 }
 
 int main()
 {
-    // ç®€å•è‡ªåŠ¨ç±»å‹æ¨æ–­
-    //     auto a = 123;
-    //         cout << "type of a is " << typeid(a).name() << endl;
-    //             auto s("fred");
-    //                 cout << "type of s is " << typeid(s).name() << endl;
-    //
-    //                     // å†—é•¿çš„ç±»å‹è¯´æ˜ï¼ˆå¦‚è¿­ä»£å™¨ï¼‰
-    //                         vector<int> vec;
-    //                             auto iter = vec.begin();
-    //                                 cout << "type of iter is " << typeid(iter).name() << endl;
-    //
-    //                                     // ä½¿ç”¨æ¨¡æ¿æŠ€æœ¯æ—¶ï¼Œå¦‚æœæŸä¸ªå˜é‡çš„ç±»å‹ä¾èµ–äºæ¨¡æ¿å‚æ•°ï¼Œä½¿ç”¨autoç¡®å®šå˜é‡ç±»å‹
-    //                                         add(101, 1.1);
-    //                                         }
+    // ¼òµ¥×Ô¶¯ÀàĞÍÍÆ¶Ï
+    auto a = 123;
+    cout << "type of a is " << typeid(a).name() << endl;
+    auto s("fred");
+    cout << "type of s is " << typeid(s).name() << endl;
+
+    // Èß³¤µÄÀàĞÍËµÃ÷£¨Èçµü´úÆ÷£©
+    vector<int> vec;
+    auto iter = vec.begin();
+    cout << "type of iter is " << typeid(iter).name() << endl;
+
+    // Ê¹ÓÃÄ£°å¼¼ÊõÊ±£¬Èç¹ûÄ³¸ö±äÁ¿µÄÀàĞÍÒÀÀµÓÚÄ£°å²ÎÊı£¬Ê¹ÓÃautoÈ·¶¨±äÁ¿ÀàĞÍ
+    add(101, 1.1);
+
+    auto value = add2(101, 1.1);
+    cout << "type of value is " << typeid(value).name() << endl;
+
+	auto value2 = add3(101, 10);
+    cout << "type of value2 is " << typeid(value2).name() << endl;
+}
